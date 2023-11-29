6addcedb58f70f2ae3962b0f8a797bf71a514bb6
louiss0 <louisshelton0gmail.com>
2023-11-29 14:42:31 -0500

    chore: setup uno css and markdoc

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -1,6 +1,15 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+import UnoCSS from 'unocss/astro'
+import markdoc from "@astrojs/markdoc"
 export default defineConfig({
+  integrations: [
+    UnoCSS({
+      includeReset: true
+    }),
+    markdoc({
+      ignoreIndentation: true
+    })
+  ],
   outDir: '../../dist/apps/blog',
 });
