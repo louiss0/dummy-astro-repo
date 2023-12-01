c6ba8d2d8df603c6f7356993047492928ead438c
louiss0 <louisshelton0gmail.com>
2023-12-01 12:13:49 -0500

    feat: add utils from lib to project utils file.

--- apps/blog/src/utils.ts
+++ apps/blog/src/utils.ts
@@ -1,4 +1,4 @@
-import { type CollectionKey, type CollectionEntry } from "astro:content";
+import type { CollectionKey, CollectionEntry } from "astro:content";
 import { COLLECTION_NAMES, type AllCollectionNames, type AllCollectionNamesValues } from "./collections";
 import { getCollections } from "@forastro/utilities";
 
@@ -47,4 +47,33 @@ export const attemptToCreateSpacedStringFromLowerCaseAndDashedFileNameByCapturin
             .join(' ')
 
     }
+
+
+export const isSpaced = (value: string) => /\s/gm.test(value)
+
+export const aWordWithOnlyLettersAndStartsWithUppercase =
+    (value: string) => /^(?:[A-Z][a-z]*)$/.test(value)
+
+export const isCapitalized = (value: string) =>
+    value
+        .split(" ")
+        .every(aWordWithOnlyLettersAndStartsWithUppercase)
+
+
+export function isAPunctualParagraph(value: string) {
+
+    const isProperParagraphRE =
+        /^(?<first_word>[A-Z][a-z]+)(?<middle_words>(?:\s?[\w#?.@,!"':;-]+)+)(?<last_word>\s\w+[.!?])$/g
+
+
+    return isProperParagraphRE.test(value)
+
+
+}
+
+export const curryFunctionToCallWithOppositeBoolean = (
+    cb: (...args: Array<NonNullable<unknown>>) => boolean) =>
+    (...args: Array<NonNullable<unknown>>) => !cb(...args)
+
+
 
